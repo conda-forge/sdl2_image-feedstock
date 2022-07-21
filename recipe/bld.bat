16e98d9eb3306f151b8b43a3b4ea1d107c9a709d
H. Vetinari <h.vetinari@gmx.com>
2022-07-21 19:44:10 +0200

    switch to now-native CMake builds; switch on all all available format support

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,18 +1,44 @@
-setlocal EnableDelayedExpansion
-
-copy %RECIPE_DIR%\CMakeLists.txt .\CMakeLists.txt
+@echo on
 
 :: Make a build folder and change to it.
 mkdir build
 cd build
 
 :: Configure using the CMakeFiles
-cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" -DCMAKE_BUILD_TYPE:STRING=Release .. 
-if errorlevel 1 exit 1
+
+:: full list of formats as of:
+:: https://github.com/libsdl-org/SDL_image/blob/release-2.6.0/CMakeLists.txt#L63-L79
+:: disabled JXL because it's not yet in conda-forge
+cmake -G Ninja ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DBUILD_SHARED_LIBS=ON ^
+    -DSDL2IMAGE_DEPS_SHARED=ON ^
+    -DSDL2IMAGE_VENDORED=OFF ^
+    -DSDL2IMAGE_AVIF=ON ^
+    -DSDL2IMAGE_BMP=ON ^
+    -DSDL2IMAGE_GIF=ON ^
+    -DSDL2IMAGE_JPG=ON ^
+    -DSDL2IMAGE_JPG_SAVE=ON ^
+    -DSDL2IMAGE_JXL=OFF ^
+    -DSDL2IMAGE_LBM=ON ^
+    -DSDL2IMAGE_PCX=ON ^
+    -DSDL2IMAGE_PNG=ON ^
+    -DSDL2IMAGE_PNG_SAVE=ON ^
+    -DSDL2IMAGE_PNM=ON ^
+    -DSDL2IMAGE_QOI=ON ^
+    -DSDL2IMAGE_SVG=ON ^
+    -DSDL2IMAGE_TGA=ON ^
+    -DSDL2IMAGE_TIF=ON ^
+    -DSDL2IMAGE_WEBP=ON ^
+    -DSDL2IMAGE_XCF=ON ^
+    -DSDL2IMAGE_XPM=ON ^
+    -DSDL2IMAGE_XV=ON ^
+    ..
+if %ERRORLEVEL% neq 0 exit 1
 
 :: Build!
-nmake
-if errorlevel 1 exit 1
+cmake --build .
+if %ERRORLEVEL% neq 0 exit 1
 
-nmake install
-if errorlevel 1 exit 1
+cmake --install . --prefix %LIBRARY_PREFIX%
+if %ERRORLEVEL% neq 0 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
+set -ex
 
-# Make sure TravisCI can find SDL2
-if [ `uname` == Darwin ]; then
-	export LDFLAGS="${LDFLAGS} -Wl,-rpath,$PREFIX/lib"
-fi
+mkdir build
+cd build
 
-./configure --disable-dependency-tracking --enable-imageio=no --prefix=${PREFIX}
-make install
+# full list of formats as of:
+# https://github.com/libsdl-org/SDL_image/blob/release-2.6.0/CMakeLists.txt#L63-L79
+# disabled JXL because it's not yet in conda-forge
+cmake -G Ninja \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=ON \
+    -DSDL2IMAGE_DEPS_SHARED=ON \
+    -DSDL2IMAGE_VENDORED=OFF \
+    -DSDL2IMAGE_AVIF=ON \
+    -DSDL2IMAGE_BMP=ON \
+    -DSDL2IMAGE_GIF=ON \
+    -DSDL2IMAGE_JPG=ON \
+    -DSDL2IMAGE_JPG_SAVE=ON \
+    -DSDL2IMAGE_JXL=OFF \
+    -DSDL2IMAGE_LBM=ON \
+    -DSDL2IMAGE_PCX=ON \
+    -DSDL2IMAGE_PNG=ON \
+    -DSDL2IMAGE_PNG_SAVE=ON \
+    -DSDL2IMAGE_PNM=ON \
+    -DSDL2IMAGE_QOI=ON \
+    -DSDL2IMAGE_SVG=ON \
+    -DSDL2IMAGE_TGA=ON \
+    -DSDL2IMAGE_TIF=ON \
+    -DSDL2IMAGE_WEBP=ON \
+    -DSDL2IMAGE_XCF=ON \
+    -DSDL2IMAGE_XPM=ON \
+    -DSDL2IMAGE_XV=ON \
+    ..
+
+cmake --build .
+cmake --install . --prefix $PREFIX
